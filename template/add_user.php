21e4989a7861b3930d146547bcddbed5527fb108
hunter eyes eg <wadedy2020@gmail.com>
2016-10-08 21:46:39 +0200

    Create add_user.php

---
+++ template/add_user.php
@@ -0,0 +1,29 @@
+<!DOCTYPE>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Add new user </title>
+	<style>
+				h4{
+			color: red;
+			background: ;
+			background-color: yellow;
+			width: 50px;
+			border-left: 5px;
+
+		}
+	</style>
+</head>
+<body>
+<a href="index.php"><h4>Home</h4></a>
+<form action="" method="post">
+	<input type="text" name="username" placeholder="username" /><br>
+	<input type="password" name="password" placeholder="password" /><br>
+	<input type="email" name="email" placeholder="email" /><br>
+	<input type="submit" name="register"  value="Add new user" />
+</form>
+
+</body>
+</html>
