55182df1cd1e505323f61857f760b52dd0e4798a
hunter eyes eg <wadedy2020@gmail.com>
2016-10-08 21:45:38 +0200

    Create config.php

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php
+define('SERVER_NAME','localhost'); //server name
+define('DB_NAME','awc-dev'); // database name
+define('DB_USER','root'); // database user
+define('DB_PASSWORD',''); // database password
+
